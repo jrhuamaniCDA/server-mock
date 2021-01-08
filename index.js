8ad8cffe2f77f7131dcf7ad6999127e4bd230a98
Junior Huamani <HJunior@proveedor.teco.com.ar>
2021-01-08 13:14:29 -0300

    INIT

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const dataJson = require("./json/mock.json")
+
+app.get("/mock", function (req, res) {
+  res.status(404).send(dataJson);
+});
+
+app.get("/", function (req, res) {
+  respuesta = {
+    error: true,
+    codigo: 200,
+    mensaje: "Punto de inicio",
+  };
+  res.send(respuesta);
+});
+
+app.listen(3000, () => {
+  console.log("El servidor está inicializado en el puerto 3000");
+});
